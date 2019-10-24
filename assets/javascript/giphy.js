95865c3cfcf574e991193ecf2fcd1d74755ab9a4
dan demeo <chronos937@gmail.com>
2019-10-24 02:03:31 -0400

    added functions to create buttons to search for gifs on click, floated searchSpace to the right. next have  to attach ajax calls to the gifbuttons.

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,11 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    var gifs = ["cats","dogs","koalas","bears","birds","barbarians"];
     var searchTerm;
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=1vvJ8zUB4rh6oQdjqvrFH1yefGrtirh7&q=&limit=25&offset=0&rating=G&lang=en&q=" + searchTerm;
+
+
+    function createButtons() {
+        $("#buttonSpace").empty();
+        for (i = 0; i < gifs.length; i++) {
+            var a = $("<button>");
+            a.addClass("gifChoice");
+            a.attr("data-name", gifs[i]);
+            a.text(gifs[i]);
+            $("#buttonSpace").append(a);
+        }
+    }
+
+    $("#addGif").on("click", function (event) {
+        event.preventDefault();
+        var gifName = $("#searchInput").val().trim();
+
+        gifs.push(gifName);
+        createButtons();
+    })
+
 
     $.ajax({
         url: queryURL,
         method: "GET",
-    }).then(function(response){
-        
+    }).then(function (response) {
+
     })
+    createButtons();
 })
